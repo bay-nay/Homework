c8e7dca57e86184ff980f7e585f7212e4790f9f9
bay-nay <enubula123@gmail.com>
2023-10-29 22:40:36 +0700

    lmao

---
+++ homework-2.js
@@ -0,0 +1,15 @@
+const basket =[["orange",20],["sword",100],["money",10000]]
+console.log(`I have ${basket[0][1]} ${basket[0][0]}`)
+
+for (i = 0 ; i < basket.length;i++) {
+    console.log(basket[i]); 
+}
+
+
+var myDog ={
+    "name":"Mốc",
+    "leg": 9,
+    "friend":"nightmare"
+}
+myDog.name="Say"
+
