336650e5849b7b21643c7eb96bcd18920de029e9
bay-nay <enubula123@gmail.com>
2023-11-10 21:53:10 +0700

    HEHEHEHEHEHEHEH

---
+++ homeworklesson4/sad.js
@@ -0,0 +1,34 @@
+const e1=function change() {
+    const E1 = document.getElementById("1")
+    E1.style.color="white"
+}
+
+setTimeout(e1,2000)
+
+const e2=function change() {
+    const E1 = document.getElementById("2")
+    E1.style.color="white"
+}
+
+setTimeout(e2,4000)
+
+const e3=function change() {
+    const E1 = document.getElementById("3")
+    E1.style.color="white"
+}
+
+setTimeout(e3,6000)
+
+const e4=function change() {
+    const E1 = document.getElementById("4")
+    E1.style.color="white"
+}
+
+setTimeout(e4,8000)
+
+const e5=function change() {
+    const E1 = document.getElementById("5")
+    E1.style.color="white"
+}
+
+setTimeout(e5,10000)
